ef49e43131af73bbc2782c782afbf9998d366038
Tiago Antão <tiagoantao@MBP-de-Tiago.fc.ul.pt>
2023-10-11 17:04:04 +0100

    Create initial Block structure

---
+++ block.py
@@ -0,0 +1,14 @@
+class Block:
+    """
+    Class that represents a block containing:
+    - Hash of the (block) parent hash
+    - Set of transactions
+    - Block length
+    """
+
+    length = 0
+
+    def __init__(self):
+        self.parent_hash = ""
+        self.transactions = []
+        self.length = Block.length + 1
