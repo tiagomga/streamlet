7b3596e7268b4c596e2b52ae155453554181c4c0
Tiago Antão <tiagoantao@MBP-de-Tiago.fc.ul.pt>
2023-10-17 16:54:31 +0100

    Create send_value() function to simulate a transaction

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import socket
 import sys
 
 HOST = "127.0.0.1"
-PORT = 5000
+PORT = 50000
 
 def print_operations():
     print("OPERATIONS:")
@@ -10,17 +10,20 @@ def print_operations():
     print("   2 - OTHER OPERATION")
     print("   3 - EXIT")
 
-def send_value(value):
-    #TODO
-    return
+def send_value():
+    receiver_id = input("Receiver ID: ")
+    value = input("Value: ")
+    data = f"({receiver_id}, {value})".encode()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(data)
 
 def main():
     print_operations()
     while True:
         user_input = input(">>> ")
         if user_input == "1":
-            #TODO
-            return
+            send_value()
         elif user_input == "2":
             #TODO
             return
