2739d4ebcb3e259a7e0f29b4bdd3cb236177788d
Tiago Antão <tiagoantao@MBP-de-Tiago.fc.ul.pt>
2023-10-26 18:47:10 +0100

    Update and organize constructor variables

--- server.py
+++ server.py
@@ -1,16 +1,32 @@
 import socket
 import rsa
+from multiprocessing import Process, Queue
 from block import Block
+from communicationsystem import CommunicationSystem
 
 class Server:
 
     total_servers = 0
     log = []
 
-    def __init__(self):
-        self.id = 0
+    def __init__(self, servers_configuration, id):
+        # Server information
+        self.id = id
+        self.servers_configuration = servers_configuration
+        self.communication = CommunicationSystem(id, servers_configuration)
+        self.ip = servers_configuration[id][0]
+        self.port = servers_configuration[id][1]
+        self.socket = servers_configuration[id][2]
+        self.socket.bind((self.ip, self.port))
+
+        # Protocol information
         self.epoch = 0
         self.public_key, self.private_key = rsa.newkeys(512)
+
+        # Queues
+        self.receive_queue = Queue()
+        self.send_queue = Queue()
+
         Server.total_servers += 1
 
     def get_epoch_leader(self):
