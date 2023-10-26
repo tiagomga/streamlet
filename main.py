d89c97305f54c137ce499dac21eeb32dd71f11f4
Tiago Antão <tiagoantao@MBP-de-Tiago.fc.ul.pt>
2023-10-26 18:23:39 +0100

    Fix error in socket creation

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ HOST = "127.0.0.1"
 PORT = 10000
 
 SERVERS_CONFIGURATION = {
-    0: (HOST, PORT, socket.socket(socket.AF_INET, socket.SOCK_STREAM).bind((HOST, PORT))),
-    1: (HOST, PORT+1, socket.socket(socket.AF_INET, socket.SOCK_STREAM).bind((HOST, PORT+1))),
-    2: (HOST, PORT+2, socket.socket(socket.AF_INET, socket.SOCK_STREAM).bind((HOST, PORT+2))),
-    3: (HOST, PORT+3, socket.socket(socket.AF_INET, socket.SOCK_STREAM).bind((HOST, PORT+3)))
+    0: (HOST, PORT, socket.socket(socket.AF_INET, socket.SOCK_STREAM)),
+    1: (HOST, PORT+1, socket.socket(socket.AF_INET, socket.SOCK_STREAM)),
+    2: (HOST, PORT+2, socket.socket(socket.AF_INET, socket.SOCK_STREAM)),
+    3: (HOST, PORT+3, socket.socket(socket.AF_INET, socket.SOCK_STREAM))
 }
 
 def main():
+    print(SERVERS_CONFIGURATION)
     if len(sys.argv) != 2:
         print("Usage: python3 main.py <server_id>", file=sys.stderr, flush=True)
         sys.exit(1)
