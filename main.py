d5fb9c31ad77ee2086d07c724d03598da82e7635
Tiago Antão <tiagoantao@MBP-de-Tiago.fc.ul.pt>
2023-10-26 17:49:44 +0100

    Add server configurations

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+import socket
+import sys
+from server import Server
+
+HOST = "127.0.0.1"
+PORT = 10000
+
+SERVERS_CONFIGURATION = {
+    0: (HOST, PORT, socket.socket(socket.AF_INET, socket.SOCK_STREAM).bind((HOST, PORT))),
+    1: (HOST, PORT+1, socket.socket(socket.AF_INET, socket.SOCK_STREAM).bind((HOST, PORT+1))),
+    2: (HOST, PORT+2, socket.socket(socket.AF_INET, socket.SOCK_STREAM).bind((HOST, PORT+2))),
+    3: (HOST, PORT+3, socket.socket(socket.AF_INET, socket.SOCK_STREAM).bind((HOST, PORT+3)))
+}
+
 def main():
-    return
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <server_id>", file=sys.stderr, flush=True)
+        sys.exit(1)
+    try:
+        server_id = int(sys.argv[1])
+    except ValueError:
+        sys.exit(1)
+    server = Server(SERVERS_CONFIGURATION, server_id)
 
 if __name__ == "__main__":
     main()
