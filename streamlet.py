7502f7a67947c5c1ccf7f706f29763821fa0e30c
Tiago Antão <tiagoantao@MBP-de-Tiago.fc.ul.pt>
2023-11-16 15:13:59 +0000

    Develop start_new_epoch() function

--- streamlet.py
+++ streamlet.py
@@ -3,16 +3,21 @@ class Streamlet:
     Streamlet protocol.
     """
 
-    def __init__(self):
+    def __init__(self, server_id, num_replicas=4):
         """
         Constructor.
         """
+        self.server_id = server_id
         self.epoch = 0
+        self.num_replicas = num_replicas
 
 
-    def start_new_epoch(self):
-        #TODO
-        pass
+    def start_new_epoch(self, block):
+        epoch_leader = self.get_epoch_leader()
+        if epoch_leader == self.server_id:
+            self.propose(block)
+        else:
+            self.vote(block)
 
 
     def propose(self, block):
@@ -28,3 +33,7 @@ class Streamlet:
     def finalize(self, block):
         #TODO
         pass
+
+
+    def get_epoch_leader(self):
+        return self.epoch % self.num_replicas
