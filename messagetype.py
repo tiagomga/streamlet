db3d366e4665201b27130f1e3999c94b774eb233
Tiago Antão <tiagoantao@MBP-de-Tiago.fc.ul.pt>
2023-11-16 16:30:52 +0000

    Complete documentation

--- messagetype.py
+++ messagetype.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 class MessageType(Enum):
     """
-    Class than contains the different types of message.
+    Class than contains the different types of messages exchanged in the protocol.
     """
     REQUEST = 0
     PROPOSE = 1
