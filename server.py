7a4e786f9ec0fcf73c6371dfc775b0743e42935d
Tiago Antão <tiagoantao@MBP-de-Tiago.fc.ul.pt>
2023-11-16 14:23:23 +0000

    Remove unnecessary code regarding sockets

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
-import socket
 import rsa
 from multiprocessing import Process, Queue
-from block import Block
 from communicationsystem import CommunicationSystem
+import time
 
 class Server:
 
@@ -11,10 +10,6 @@ class Server:
         self.id = id
         self.servers_configuration = servers_configuration
         self.communication = CommunicationSystem(id, servers_configuration)
-        self.ip = servers_configuration[id][0]
-        self.port = servers_configuration[id][1]
-        self.socket = servers_configuration[id][2]
-        self.socket.bind((self.ip, self.port))
 
         # Protocol information
         self.epoch = 0
@@ -28,17 +23,10 @@ class Server:
     def get_epoch_leader(self):
         return self.get_current_epoch() % Server.total_servers
     
+
     def get_current_epoch(self):
         return self.epoch
     
+    
     def run(self):
-        receiver_process = Process(target=self.receive)
-        receiver_process.start()
-
-        protocol_process = Process(target=self.start_new_epoch)
-        protocol_process.start()
-
-
-    def receive(self):
-        print(f"Server {self.id} is ready to receive.")
-        self.communication.receive(self.receive_queue)
+        self.communication.start()
