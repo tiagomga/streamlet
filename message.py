0efba49a175c8eda76c59783d5589c125b6e0a4c
Tiago Antão <tiagoantao@MBP-de-Tiago.fc.ul.pt>
2023-11-16 17:03:12 +0000

    Add __str__()

--- message.py
+++ message.py
@@ -7,3 +7,7 @@ class Message:
         self.type = type
         self.content = content
         self.sender_id = sender_id
+    
+
+    def __str__(self):
+        return f"({self.type}, {self.content}, {self.sender_id})"
