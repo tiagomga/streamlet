2e66527810f4b20c49f0f32f63335fd01815dd09
Tiago Antão <tiagoantao@MBP-de-Tiago.fc.ul.pt>
2023-11-17 14:59:04 +0000

    Add get_latest_notarized_block() function

--- blockchain.py
+++ blockchain.py
@@ -1,3 +1,5 @@
+from blockstatus import BlockStatus
+
 class Blockchain:
     """
     Class that contains the blockchain structure and its operations.
@@ -19,3 +21,18 @@ class Blockchain:
             block (Block): block to be added to the blockchain
         """
         self.chain[epoch] = block
+
+
+    def get_latest_notarized_block(self):
+        """
+        Get latest notarized block from the blockchain.
+
+        Returns:
+            Block: latest notarized block
+        """
+        epochs = list(self.chain.keys()).sort()
+        i = len(epochs) - 1
+        while i >= 0:
+            if self.chain[i].status == BlockStatus.NOTARIZED:
+                return self.chain[i]
+            i -= 1
