1f15ccc4b2232b686c5fe4039ba950edc440b9df
Tiago Antão <tiagoantao@MBP-de-Tiago.fc.ul.pt>
2023-11-17 13:41:23 +0000

    Create Blockchain class

---
+++ blockchain.py
@@ -0,0 +1,10 @@
+class Blockchain:
+    """
+    Class that contains the blockchain structure and its operations.
+    """
+
+    def __init__(self):
+        """
+        Constructor.
+        """
+        self.chain = {}
