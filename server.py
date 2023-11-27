9e4a1ffcae40c8333639d12fc895c6d612f82b0e
Tiago Antão <tiagoantao@mbp-de-tiago.lan>
2023-11-27 13:35:26 +0000

    Add test and debug function to see queue's contents

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import rsa
+import time
 from communicationsystem import CommunicationSystem
+from message import Message
 
 class Server:
 
@@ -15,3 +17,26 @@ class Server:
         Start server execution.
         """
         self.communication.start()
+        
+        # Test for communication between replicas
+        try:
+            time.sleep(5)
+            data = f"Hey (from replica {self.id})"
+            m = Message(None, data, self.id)
+            m_byte = Message.to_bytes(m)
+            self.communication.send(m_byte)
+        except Exception:
+            pass
+        self.show_queue()
+
+
+    def show_queue(self):
+        """
+        Debug method to see what's in the queue.
+        """
+        result = []
+        while not self.communication.received_queue.empty():
+            result.append(self.communication.received_queue.get().__str__())
+        for i in result:
+            self.communication.received_queue.put(i)
+        print(result)
