6dc4a2c34f3775b1b79bf41277acdbd63e20eca8
Tiago Antão <tiagoantao@mbp-de-tiago.lan>
2023-11-28 00:49:27 +0000

    Add missing parameter to create_vote() call in vote()

--- streamlet.py
+++ streamlet.py
@@ -70,7 +70,7 @@ class Streamlet:
         if not valid_block:
             raise Exception
         self.blockchain.add_block(proposed_block)
-        _vote = proposed_block.create_vote()
+        _vote = proposed_block.create_vote(self.private_key)
         vote_message = Message(MessageType.VOTE, _vote, self.server_id).to_bytes()
         self.communication.send(vote_message)
 
