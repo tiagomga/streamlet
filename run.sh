d97bbe806b83bf7dca91c5a8f7b32ef8814ef129
Tiago Antão <tiagoantao@MBP-de-Tiago.fc.ul.pt>
2024-01-16 15:47:22 +0000

    Add script to start the protocol (for MacOS)

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+osascript -e 'tell app "Terminal" to do script "python3 <path_to_streamlet_folder> 0"'
+osascript -e 'tell app "Terminal" to do script "python3 <path_to_streamlet_folder> 1"'
+osascript -e 'tell app "Terminal" to do script "python3 <path_to_streamlet_folder> 2"'
+osascript -e 'tell app "Terminal" to do script "python3 <path_to_streamlet_folder> 3"'
