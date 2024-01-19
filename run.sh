27eeb3b260ab9c1be7132b55da2a31c9ab6dac03
Tiago Antão <tiagoantao@mbp-de-tiago.lan>
2024-01-19 17:19:37 +0000

    Fix path to main.py

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-osascript -e 'tell app "Terminal" to do script "python3 <path_to_streamlet_folder> 0"'
-osascript -e 'tell app "Terminal" to do script "python3 <path_to_streamlet_folder> 1"'
-osascript -e 'tell app "Terminal" to do script "python3 <path_to_streamlet_folder> 2"'
-osascript -e 'tell app "Terminal" to do script "python3 <path_to_streamlet_folder> 3"'
+osascript -e 'tell app "Terminal" to do script "python3 <path_to_streamlet_folder>/main.py 0"'
+osascript -e 'tell app "Terminal" to do script "python3 <path_to_streamlet_folder>/main.py 1"'
+osascript -e 'tell app "Terminal" to do script "python3 <path_to_streamlet_folder>/main.py 2"'
+osascript -e 'tell app "Terminal" to do script "python3 <path_to_streamlet_folder>/main.py 3"'
