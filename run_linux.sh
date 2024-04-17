e90b2dd5ea59ad2f6beee3ef1699cfc49db79fb5
Tiago Antão <tiagoantao@mbp-de-tiago.lan>
2024-04-17 15:16:54 +0100

    Change the directory of the terminal before the protocol is executed

--- run_linux.sh
+++ run_linux.sh
@@ -2,5 +2,5 @@
 FOLDER_PATH=$(pwd)
 for i in {0..3}
 do
-    gnome-terminal -- sh "python3 $FOLDER_PATH/main.py $i"
+    gnome-terminal -- sh "cd '$FOLDER_PATH' && python3 main.py $i"
 done
--- run_macos.sh
+++ run_macos.sh
@@ -2,5 +2,5 @@
 FOLDER_PATH=$(pwd)
 for i in {0..3}
 do
-    osascript -e "tell app \"Terminal\" to do script \"python3 '$FOLDER_PATH'/main.py $i\""
+    osascript -e "tell app \"Terminal\" to do script \"cd '$FOLDER_PATH' && python3 main.py $i\""
 done
