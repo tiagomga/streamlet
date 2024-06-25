b01437d1a4116f181b9246e1d25a3927dc1bff1f
Tiago Antão <tiagoantao@mbp-de-tiago.lan>
2024-06-25 16:10:53 +0100

    Update main.py path

--- run_macos.sh
+++ run_macos.sh
@@ -2,5 +2,5 @@
 FOLDER_PATH=$(pwd)
 for i in {0..3}
 do
-    osascript -e "tell app \"Terminal\" to do script \"cd '$FOLDER_PATH' && python3 main.py $i\""
+    osascript -e "tell app \"Terminal\" to do script \"cd '$FOLDER_PATH' && python3 src/main.py $i\""
 done
