f9b41d3715c6d07e75a0b5e91daff2ed8d4b2034
Tiago Antão <tiagoantao@mbp-de-tiago.lan>
2024-07-08 16:37:14 +0100

    Create Certificate class

---
+++ src/certificate.py
@@ -0,0 +1,20 @@
+import rsa
+import pickle
+
+class Certificate:
+    """
+    Certificate for the freshest notarized block, containing a
+    quorum of votes.
+    """
+
+    def __init__(self, block=None):
+        if block is None:
+            self.epoch = 0
+            self.block_hash = ""
+            self.votes = []
+        else:
+            self.epoch = block.get_epoch()
+            self.block_hash = block.get_hash()
+            self.votes = []
+            for sender, vote in block.get_votes():
+                self.votes.append((sender, vote.get_signature()))
