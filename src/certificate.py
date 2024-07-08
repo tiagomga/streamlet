09c67e30a550942390d859fc33c8f9c0fafe2219
Tiago Antão <tiagoantao@mbp-de-tiago.lan>
2024-07-08 16:38:46 +0100

    Add check_validity() function

--- src/certificate.py
+++ src/certificate.py
@@ -22,3 +22,18 @@ class Certificate:
 
     def extends_freshest_chain(self, block):
         return self.epoch == block.get_epoch() and self.block_hash == block.get_hash()
+
+
+    def check_validity(self, block, public_keys, min_votes):
+        valid_votes = 0
+        iterated_senders = []
+        for sender, signature in self.votes:
+            if sender not in iterated_senders:
+                iterated_senders.append(sender)
+                try:
+                    hash_algorithm = rsa.verify(block.to_bytes(), signature, public_keys[sender])
+                    if hash_algorithm == 'SHA-256':
+                        valid_votes += 1
+                except rsa.VerificationError:
+                    continue
+        return valid_votes >= min_votes
