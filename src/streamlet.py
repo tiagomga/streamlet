cc5dd459cde253980799cfaf2cdde0f8d6f16689
Tiago Antão <tiagoantao@mbp-de-tiago.lan>
2024-08-13 19:29:31 +0100

    Remove exception handling when getting block in get_message() (it is handled in CommunicationSystem)

--- src/streamlet.py
+++ src/streamlet.py
@@ -3,7 +3,6 @@ import time
 import logging
 from multiprocessing import Value
 from typing import NoReturn
-from pickle import PickleError
 from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
 from block import Block
 from blockstatus import BlockStatus
@@ -216,14 +215,7 @@ class Streamlet:
                 raise TimeoutError
             message = self.communication.get_message(remaining_time)
             sender = message.get_sender()
-            try:
-                block = Block.from_bytes(message.get_content())
-            except PickleError:
-                logging.error("Block cannot be deserialized.\n")
-                continue
-            if not block.check_type_integrity():
-                logging.error("Block attributes do not contain the correct type(s).\n")
-                continue
+            block = message.get_content()
             block_epoch = block.get_epoch()
             logging.debug(f"Message type - {message.get_type()}\n\n")
 
