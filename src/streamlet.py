a9a0947b07546f2b301e31cb16aebbcdfe960c40
Tiago Antão <tiagoantao@mbp-de-tiago.lan>
2024-08-15 04:50:23 +0100

    Restructure application to event-based design
    - Update code design to event-based: process messages as they are delivered; messages are processed accordingly to their type.
    - Rename get_messages() to process_messages().
    - In start_new_epoch(), propose() if the server is the leader and then process messages with process_messages().
    - Update process_messages() documentation.
    - Extract process_proposal() from vote(): reduces redundancy and code repetition from former vote() and get_message(); handling proposals from past and current epochs is similar.
    - Create process_vote() from notarize(): reduces redundancy and code repetition from former notarize() and get_message(); handling votes for blocks from past and current epochs is similar.
    - Add documentation to process_proposal() and process_vote().
    - Delete extra newline characters from some logging messages: more uniform spacing in the output.
    - Add new logging messages to notify when blocks are notarized and finalized.
    - Change the content of some logging messages.
    - Improve some code comments: make comments clearer and concise.

--- src/streamlet.py
+++ src/streamlet.py
@@ -43,10 +43,7 @@ class Streamlet:
         epoch_leader = self.get_epoch_leader()
         if epoch_leader == self.server_id:
             self.propose()
-        else:
-            self.vote(epoch_leader, start_time)
-        self.notarize(start_time)
-        self.finalize()
+        self.process_messages(start_time)
 
 
     def propose(self) -> None:
@@ -83,13 +80,18 @@ class Streamlet:
         self.send_message(MessageType.PROPOSE, proposed_block)
 
 
-    def vote(self, leader_id: int, start_time: float) -> None:
+    def process_proposal(self, proposed_block: Block, leader_id: int) -> None:
         """
-        Vote for the proposed block.
+        Process proposal.
+
+        Args:
+            proposed_block (Block): block proposal
+            leader_id (int): leader of the epoch when block was proposed
+
+        Raises:
+            ProtocolError: raises error when block is not valid or does not
+                extend the longest notarized chain
         """
-        # Get proposed block for the current epoch
-        proposer_id, proposed_block = self.get_message(start_time)
-        
         # Get leader's public key
         leader_public_key = self.servers_public_key[leader_id]
 
@@ -98,51 +100,55 @@ class Streamlet:
 
         # Check if proposed block extends from one of the longest notarized chains
         longest_notarized_block = proposed_block.extends_from(longest_notarized_blocks)
-        
+
         if longest_notarized_block is None:
             raise ProtocolError
 
         # Check if the proposed block is valid
-        valid_block = proposed_block.check_validity(leader_public_key, self.epoch.value)
+        valid_block = proposed_block.check_validity(leader_public_key)
         if not valid_block:
             raise ProtocolError
-
-        # Add leader's vote to the proposed block
-        proposed_block.add_leader_vote(proposer_id)
         
-        # Create vote for the proposed block using server's private key
-        _vote = proposed_block.create_vote(self.private_key)
-        proposed_block.add_vote((self.server_id, _vote))
+        # Add leader's vote to the proposed block
+        proposed_block.add_leader_vote(leader_id)
 
         # Add proposed block to server's blockchain
         proposed_block.set_parent_epoch(longest_notarized_block.get_epoch())
         self.blockchain.add_block(proposed_block)
+        logging.debug(f"New block proposal for epoch {proposed_block.get_epoch()} (proposer: {leader_id}).\n")
+
+
+    def vote(self, proposed_block: Block) -> None:
+        """
+        Vote for the proposed block.
+        """
+        # Create vote for the proposed block using server's private key
+        vote_block = proposed_block.create_vote(self.private_key)
+        proposed_block.add_vote((self.server_id, vote_block))
 
         # Send vote to every server participating in the protocol
-        self.send_message(MessageType.VOTE, _vote)
+        self.send_message(MessageType.VOTE, vote_block)
 
 
-    def notarize(self, start_time: float) -> None:
+    def process_vote(self, vote: Block, block: Block, voter: int) -> None:
         """
-        Notarize block after getting 2f + 1 votes.
-        """
-        # Get proposed block for the current epoch from server's blockchain
-        proposed_block = self.blockchain.get_block(self.epoch.value)
+        Process vote.
 
-        # For every vote, check its signature validity
-        # If it is valid, add vote to the proposed block
-        num_votes = len(proposed_block.get_votes())
-        num_votes_left = 2*self.f+1 - num_votes
-        for i in range(num_votes_left):
-            sender, vote = self.get_message(start_time)
-            valid_vote = Block.check_vote(vote, proposed_block, self.servers_public_key[sender])
-            if valid_vote:
-                num_votes += 1
-                proposed_block.add_vote((sender, vote))
+        Args:
+            vote (Block): vote
+            block (Block): block
+            voter (int): ID of the server that voted
+        """
+        # Add valid (and not repeated) votes to the block
+        if Block.check_vote(vote, block, self.servers_public_key[voter]):
+            block.add_vote((voter, vote))
+            logging.debug(f"New vote for block from epoch {block.get_epoch()} (voter: {voter}).\n")
         
-        # Check if there are sufficient valid votes; then, notarize the proposed block
-        if num_votes >= 2*self.f+1:
-            proposed_block.notarize()
+        # Notarize the block if there are sufficient valid votes (2f+1)
+        if len(block.get_votes()) >= 2*self.f+1 and block.get_status() == BlockStatus.PROPOSED:
+            block.notarize()
+            logging.info(f"Block from epoch {block.get_epoch()} was notarized.\n")
+            self.finalize()
 
 
     def finalize(self) -> None:
@@ -152,9 +158,10 @@ class Streamlet:
         """
         if self.epoch.value > 2:
             finalized_blocks = self.blockchain.finalize()
-            # Execute clients' transactions
-            # if finalized_blocks:
-            #     execute_transactions(finalized_blocks)
+            if finalized_blocks:
+                logging.info(f"Blocks from epochs {', '.join([str(block.get_epoch()) for block in finalized_blocks])} were finalized.\n")
+                # Execute clients' transactions
+                # execute_transactions(finalized_blocks)
 
 
     def get_epoch_leader(self) -> int:
@@ -179,20 +186,18 @@ class Streamlet:
                 start_time = time.time()
                 self.start_new_epoch()
             except TimeoutError:
-                logging.info("Epoch ended abruptly: a timeout was triggered.\n")
-            except ProtocolError:
-                logging.info("Epoch ended abruptly: invalid block.\n")
+                logging.info("Timeout triggered: epoch reached its end.\n")
             finally:
-                logging.info(f"Blockchain - {self.blockchain}\n\n")
+                logging.info(f"Blockchain - {self.blockchain}\n")
                 end_time = time.time()
                 epoch_duration = end_time - start_time
                 if epoch_duration < self.epoch_duration:
                     time.sleep(self.epoch_duration - epoch_duration)
 
 
-    def get_message(self, start_time: float) -> tuple:
+    def process_messages(self, start_time: float) -> None:
         """
-        Get message. Returns proposal or vote for the current epoch.
+        Process received messages.
         
         More details:
         - Ignores proposals and votes for epochs higher than the current epoch.
@@ -205,9 +210,6 @@ class Streamlet:
 
         Raises:
             TimeoutError: error is raised when epoch duration is exceeded
-
-        Returns:
-            tuple: tuple with proposal or vote, along with sender's id
         """
         while True:
             remaining_time = self.epoch_duration - (time.time() - start_time)
@@ -217,53 +219,37 @@ class Streamlet:
             sender = message.get_sender()
             block = message.get_content()
             block_epoch = block.get_epoch()
-            logging.debug(f"Message type - {message.get_type()}\n\n")
+            logging.debug(f"Message type - {message.get_type()}\n")
 
-            # Current epoch - return propose message if proposed block is new to the blockchain
-            # Past epochs - add to the blockchain if proposed block is valid and new to the blockchain
+            # Add new valid proposed blocks to the blockchain
+            # Vote for the block, if block was received in the current epoch
             if message.get_type() == MessageType.PROPOSE:
                 if block_epoch <= self.epoch.value:
                     # Check if the proposer's ID matches the leader's ID
-                    if sender != self.epoch_leaders[block_epoch]:
-                        continue
-                    try:
-                        self.blockchain.get_block(block_epoch)
-                    except KeyError:
-                        # Echo received proposal
-                        self.send_message(MessageType.ECHO, message)
-                        if block_epoch == self.epoch.value:
-                            logging.debug(f"New proposal (epoch: {self.epoch.value} | proposer: {sender})\n\n")
-                            return (sender, block)
-                        else:
-                            logging.debug(f"Old proposal (epoch: {block_epoch} | proposer: {sender})\n\n")
-                            longest_notarized_blocks = self.blockchain.get_longest_notarized_blocks()
-                            longest_notarized_block = block.extends_from(longest_notarized_blocks)
-                            if longest_notarized_block:
-                                valid_block = block.check_validity(self.servers_public_key[sender], block_epoch)
-                                if valid_block:
-                                    block.add_leader_vote(sender)
-                                    block.set_parent_epoch(longest_notarized_block.get_epoch())
-                                    self.blockchain.add_block(block)
+                    if sender == self.epoch_leaders[block_epoch]:
+                        try:
+                            self.blockchain.get_block(block_epoch)
+                        except KeyError:
+                            # Echo received proposal
+                            self.send_message(MessageType.ECHO, message)
+                            try:
+                                self.process_proposal(block, sender)
+                            except ProtocolError:
+                                continue
+                            if block_epoch == self.epoch.value:
+                                self.vote(block)
             
-            # Return vote message if vote is new to the proposed block
+            # Add votes to blocks (from current and past epochs)
             elif message.get_type() == MessageType.VOTE:
+                # Get block for the vote's epoch from server's blockchain
                 try:
-                    blockchain_block = self.blockchain.get_block(block_epoch)
+                    proposed_block = self.blockchain.get_block(block_epoch)
                 except KeyError:
                     continue
-                if sender not in [vote[0] for vote in blockchain_block.get_votes()]:
+                if sender not in [vote[0] for vote in proposed_block.get_votes()]:
                     # Echo received vote
                     self.send_message(MessageType.ECHO, message)
-                    if block_epoch == self.epoch.value:
-                        logging.debug(f"New vote for current epoch (epoch: {self.epoch.value} | voter: {sender})\n\n")
-                        return (sender, block)
-                    else:
-                        valid_block = Block.check_vote(block, blockchain_block, self.servers_public_key[sender])
-                        if valid_block:
-                            blockchain_block.add_vote((sender, block))
-                            logging.debug(f"New vote for past epoch (epoch: {blockchain_block.get_epoch()} | voter: {sender})\n\n")
-                            if blockchain_block.get_status() == BlockStatus.PROPOSED and len(blockchain_block.get_votes()) >= 2*self.f+1:
-                                blockchain_block.notarize()
+                    self.process_vote(block, proposed_block, sender)
 
 
     def send_message(self, message_type: MessageType, content: Block | Message) -> None:
