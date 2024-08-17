cf85d803e585099534911fd3c9cddb2f77769471
Tiago Antão <tiagoantao@mbp-de-tiago.lan>
2024-08-17 03:52:34 +0100

    Simplify getting message with read_all_from_socket() in start_recovery_request()

--- src/streamlet.py
+++ src/streamlet.py
@@ -334,13 +334,8 @@ class Streamlet:
             try:
                 reply_socket, address = recovery_socket.accept()
                 reply_socket.settimeout(1)
-                message_length = self.communication.read_from_socket(reply_socket, 4)
-                message_length = struct.unpack(">I", message_length)[0]
-                data = self.communication.read_from_socket(reply_socket, message_length)
+                data = self.communication.read_all_from_socket(reply_socket)
             except TimeoutError:
-                data = None
-            except struct.error:
-                logging.error("Message length cannot be obtained.\n")
                 data = None
             if data:
                 reply_message = Message.from_bytes(data)
