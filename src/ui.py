cffb36e8f885c61758c86046c0621c015fcff873
Tiago Antão <109687852+tiagomga@users.noreply.github.com>
2024-08-23 18:19:02 +0100

    Add from_bytes() function

--- src/ui.py
+++ src/ui.py
@@ -1,4 +1,6 @@
 import pickle
+import logging
+from typing import Self
 
 class UI:
     """
@@ -24,3 +26,20 @@ class UI:
 
     def to_bytes(self) -> bytes:
         return pickle.dumps((self.sequence_number, self.signature))
+
+
+    @staticmethod
+    def from_bytes(data_bytes: bytes) -> Self | None:
+        try:
+            data = pickle.loads(data_bytes)
+        except pickle.PickleError:
+            logging.error("UI cannot be unpickled.\n")
+            return None
+        try:
+            sequence_number, signature = data
+        except ValueError:
+            logging.error("Attributes cannot be unpacked from tuple.\n")
+            return None
+        if isinstance(sequence_number, int) and isinstance(signature, str):
+            return UI(sequence_number, signature)
+        logging.error("UI attributes do not contain the correct type(s).\n")
