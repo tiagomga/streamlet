38f249efa48b25287c53619f59ddec67fe1650c8
Tiago Antão <109687852+tiagomga@users.noreply.github.com>
2024-08-23 18:53:20 +0100

    Add create_ui() function

--- src/usig.py
+++ src/usig.py
@@ -8,3 +8,10 @@ class USIG:
     def __init__(self) -> None:
         self.counter = 0
         self.public_key, self.private_key = crypto.generate_keys()
+
+
+    def create_ui(self, message: Message) -> UI:
+        self.counter += 1
+        signature_content = pickle.dumps((message.to_bytes(), self.counter))
+        signature = crypto.sign(signature_content, self.private_key)
+        return UI(self.counter, signature)
