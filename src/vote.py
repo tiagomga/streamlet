20c143825aff6a27aa0317b3b71c56e04a68774f
Tiago Antão <109687852+tiagomga@users.noreply.github.com>
2024-08-27 18:38:33 +0100

    Add documentation to __init__()

--- src/vote.py
+++ src/vote.py
@@ -4,6 +4,15 @@ class Vote:
     """
 
     def __init__(self, voter: int, epoch: int, message_hash: str, ui: UI) -> None:
+        """
+        Constructor.
+
+        Args:
+            voter (int): id of the voter
+            epoch (int): epoch from the message
+            message_hash (str): hash of the message
+            ui (UI): unique identifier
+        """
         self.voter = voter
         self.epoch = epoch
         self.message_hash = message_hash
