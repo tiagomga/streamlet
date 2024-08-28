3fefb1aade2832ced16a9b46d3e3eb86269cff10
Tiago Antão <109687852+tiagomga@users.noreply.github.com>
2024-08-28 03:50:50 +0100

    Update script to launch 3 servers

--- run_linux.sh
+++ run_linux.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 FOLDER_PATH=$(pwd)
-for i in {0..3}
+for i in {0..2}
 do
     gnome-terminal -- sh "cd '$FOLDER_PATH' && python3 src/main.py $i"
 done
--- run_macos.sh
+++ run_macos.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 FOLDER_PATH=$(pwd)
-for i in {0..3}
+for i in {0..2}
 do
     osascript -e "tell app \"Terminal\" to do script \"cd '$FOLDER_PATH' && python3 src/main.py $i\""
 done
