ca1caea2e7ec5168291a7f207459957446eedc5c
Tiago Antão <109687852+tiagomga@users.noreply.github.com>
2024-09-18 22:38:51 +0100

    Add establish_connections() function

--- src/communicationsystem.py
+++ src/communicationsystem.py
@@ -2,6 +2,7 @@ import selectors
 import logging
 import socket
 import struct
+import time
 from multiprocessing import Process, Queue
 from block import Block
 from queue import Empty
@@ -150,6 +151,14 @@ class CommunicationSystem:
                     callback(key.fileobj)
         except KeyboardInterrupt:
             self.selector.close()
+
+
+    def establish_connections(self):
+        for id in list(self.configuration.keys()):
+            if self.server_id != id:
+                while self.configuration[id][2].connect_ex((self.configuration[id][0], self.configuration[id][1])) != 0:
+                    logging.debug(f"Retrying to establish connection with server {id}...")
+                    time.sleep(0.1)
 
 
     def start(self) -> None:
