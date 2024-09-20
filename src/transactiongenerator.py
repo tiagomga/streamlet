f6d04ddc91d7a7420c6862a014d446c94d06a825
Tiago Antão <109687852+tiagomga@users.noreply.github.com>
2024-09-20 17:41:58 +0100

    Add documentation to __init__()

--- src/transactiongenerator.py
+++ src/transactiongenerator.py
@@ -4,6 +4,9 @@ class TransactionGenerator:
     """
 
     def __init__(self) -> None:
+        """
+        Constructor.
+        """
         self.queue = Queue(maxsize=10)
         self.transaction_size = int(os.environ.get("TRANSACTION_SIZE")) if os.environ.get("TRANSACTION_SIZE") else 256
         self.transaction_number = int(os.environ.get("TRANSACTION_NUMBER")) if os.environ.get("TRANSACTION_NUMBER") else 100
