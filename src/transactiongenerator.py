b83f6889e9fa2aa8c72028333ffd985766d34b88
Tiago Antão <109687852+tiagomga@users.noreply.github.com>
2024-09-20 17:41:43 +0100

    Create TransactionGenerator class

---
+++ src/transactiongenerator.py
@@ -0,0 +1,11 @@
+class TransactionGenerator:
+    """
+    Class that generates transactions.
+    """
+
+    def __init__(self) -> None:
+        self.queue = Queue(maxsize=10)
+        self.transaction_size = int(os.environ.get("TRANSACTION_SIZE")) if os.environ.get("TRANSACTION_SIZE") else 256
+        self.transaction_number = int(os.environ.get("TRANSACTION_NUMBER")) if os.environ.get("TRANSACTION_NUMBER") else 100
+        self.process = Process(target=self.generate_transactions, args=(self.queue, self.transaction_size, self.transaction_number))
+        self.process.start()
